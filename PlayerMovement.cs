7fe1a18312b852ad8520da32a7ec6b83e0b12d11
nickvaningen <nickvaningen@hotmail.com>
2018-02-02 08:59:47 +0100

    dit zijn mijn scripts
    ik hooop dat het zo goed is

---
+++ PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [HideInInspector] public bool jump = false;
+    private bool grounded = false;
+    public float jumpForce = 1000f;
+
+    public float speed;
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 400.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(1);
+        }
+        
+        
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+    void FixedUpdate ()
+    {
+        if (jump)
+        {
+
+            GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpForce));
+            jump = false;
+        }
+
+    }
+    
+}
---
+++ Player_health.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_health : MonoBehaviour {
+    private float health = 24;
+    private float damage = 1;
+    private AudioSource source;
+    private bool hit = false;
+
+    void Awake()
+    {
+
+        source = GetComponent<AudioSource>();
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Fireball")
+        {
+            hit = true;
+            health -= damage;
+            Debug.Log(health);
+        }
+        if (other.collider.tag == "hand")
+        {
+            hit = true;
+            health -= damage * 2;
+            Debug.Log(health);
+        }
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(2);
+        }
+        if(hit == true)
+        {
+            GameObject[] _objects = GameObject.FindGameObjectsWithTag("Fireball");
+            for (int i = 0; i < _objects.Length; i++)
+            {
+                Physics.IgnoreCollision(_objects[i].GetComponent<Collider>(), gameObject.GetComponent<Collider>());
+            }
+        }
+    }
+}
